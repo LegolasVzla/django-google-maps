ce9cc325df751458253b4653263316770af6f1c0
Manuel Carrero <mcomercio.web@gmail.com>
2020-08-08 20:15:28 -0400

    Transforming fixtures insert to a django command

---
+++ core/api/management/commands/fixtures_insert.py
@@ -0,0 +1,16 @@
+import os
+import subprocess
+
+from django.core.management.base import BaseCommand, CommandError
+
+class Command(BaseCommand):
+    help = 'Insert all default data'
+
+    def handle(self, *args, **options):
+        try:
+            subprocess.call("python manage.py loaddata fixtures/"+'types_user_action.json',shell=True)
+
+            self.stdout.write(self.style.SUCCESS('Successfully load fixtures'))
+
+        except Exception as e:
+            self.stdout.write(self.style.ERROR('An error happened: "%s"' % str(e)))
